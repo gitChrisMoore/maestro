383aafe39c98b53c63a9eab63484ab773c8913f6
Chris Moore <chrisdmoore06@gmail.com>
2021-12-22 00:38:20 -0500

    removed excess files

--- src/routes/Signup/Signup.int.test.js
+++
@@ -1,9 +0,0 @@
-import Signup from './Signup';
-import { render, screen } from '../../test-utils';
-
-describe('Signup Int Tests', () => {
-    it('render the email page', async () => {
-        render(<Signup />);
-        expect(screen.getByTestId('email')).not.toBeNull();
-    });
-});
